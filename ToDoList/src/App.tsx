f431d288a475c4f34818c3045de227d32f28534d
Coding-jpg <1305856002@qq.com>
2024-07-10 21:13:56 +1000

    todolist completed

--- ToDoList/src/App.tsx
+++ ToDoList/src/App.tsx
@@ -1,23 +1,85 @@
-import { theme, InputContainer } from "./components/theme";
-import { TextField, Grid, Button, ThemeProvider } from "@mui/material";
 import "./App.css";
+import { Button, Checkbox, Grid, TextField } from "@mui/material";
+import { useState } from "react";
+
+type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+};
 
 function App() {
+  const [newInput, setInput] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // add new line to todo list
+  const handleAddTodo = () => {
+    if (newInput.trim() !== "") {
+      const newTodo = {
+        id: Date.now().toString(),
+        text: newInput,
+        completed: false,
+      };
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      setInput("");
+    }
+  };
+
+  const handleDeleteTodo = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleToggleComplete = (id: string, completed: boolean) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: completed } : todo
+      )
+    );
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <InputContainer>
-        <Grid container>
-          <Grid item xs={12}>
+    <div className="container">
+      <div>
+        <Grid container spacing={2} className="inputRow">
+          <Grid item xs={10}>
             <TextField
+              fullWidth
               id="outlined-basic"
-              label="input new note"
+              label="Note"
               variant="outlined"
+              value={newInput}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <Button variant="outlined">New</Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Button variant="contained" onClick={handleAddTodo}>
+              Add
+            </Button>
           </Grid>
         </Grid>
-      </InputContainer>
-    </ThemeProvider>
+        <h1 className="header">ToDo List</h1>
+        <ul className="list">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <Checkbox
+                checked={todo.completed}
+                onChange={(e) =>
+                  handleToggleComplete(todo.id, e.target.checked)
+                }
+              />
+              {todo.text}
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                Delete
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
